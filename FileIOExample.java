8f1a72bba5762a7c2a4350ed07b8e3ff71e31a20
Peter Casey <pcasey@cocc.edu>
2021-06-03 15:53:20 -0700

    Update IO

--- FileIOExample.java
+++ FileIOExample.java
@@ -1,6 +1,10 @@
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Random;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.Collections;
 
 /**
  * Write a description of class FileIOExample here.
@@ -10,14 +14,44 @@ import java.nio.file.Paths;
  */
 public class FileIOExample
 {
+    public static final Random rnd = new Random();
+    
     public static void main(String[] args) {
-        readFile("ssss");
+        writeText("numbers.txt");
+        List<Integer> nums = readFile("numbers.txt");
+        System.out.println(nums);
+        Collections.sort(nums);
+        System.out.println(nums);
     }
     
-    public static void readFile(String name) {
-        Files.lines(new File(name).toPath())
-                    .map(s -> s.trim())
-                    .filter(s -> !s.isEmpty())
-                    .forEach(System.out::println);
+    public static List<Integer> readFile(String name) {
+        List<Integer> numbers = null;
+        
+        try {
+            numbers = Files.lines(new File(name).toPath())
+                            .map(s -> s.trim())
+                            .filter(s -> !s.isEmpty())
+                            .map(s -> Integer.parseInt(s))
+                            .collect(Collectors.toList());
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
+        
+        return numbers;
+    }
+    
+    public static void writeText(String name) {
+        try {
+            PrintWriter fileout = new PrintWriter(new FileWriter(name));
+
+            for (int i = 1; i <= 10; i++) {
+                fileout.println(rnd.nextInt(100) + 1);
+            }
+            fileout.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+ 
     }
 }
